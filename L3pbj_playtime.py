49890c3d8799688781de966a206710652f948d2d
nicoleroussell <nicoleroussell@gmail.com>
2015-03-07 11:16:09 -0500

    Create L3pbj_playtime.py

---
+++ L3pbj_playtime.py
@@ -0,0 +1,31 @@
+
+>>> 
+>>> bread=10
+>>> pb=10
+>>> jelly=4
+>>> sandwich=1
+>>> sammworthofbread = bread / 2
+>>> while bread >=2 and pb>=1 and jelly >=1:
+	print "i'm making sandwich #{0}.".format(sandwich)
+	print "i have {0} sandwiches worth of bread left, {1} sandwiches worth of jelly left, and {2} sandwiches worth of pb left.".format(sammworthofbread, jelly, pb)
+	bread=bread-2
+	pb=pb-1
+	jelly=jelly-1
+	sandwich=sandwich+1
+	sammworthofbread=bread / 2
+
+	
+i'm making sandwich #1.
+i have 5 sandwiches worth of bread left, 4 sandwiches worth of jelly left, and 10 sandwiches worth of pb left.
+i'm making sandwich #2.
+i have 4 sandwiches worth of bread left, 3 sandwiches worth of jelly left, and 9 sandwiches worth of pb left.
+i'm making sandwich #3.
+i have 3 sandwiches worth of bread left, 2 sandwiches worth of jelly left, and 8 sandwiches worth of pb left.
+i'm making sandwich #4.
+i have 2 sandwiches worth of bread left, 1 sandwiches worth of jelly left, and 7 sandwiches worth of pb left.
+>>> bread=10
+>>> pb=10
+>>> jelly=4
+>>> sandwich=1
+>>> sammworthofbread=bread/2
+>>> 
